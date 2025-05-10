19ac0049f7e8f65ac5b10115df90ab42693cb2b0
glenn <glenn.reumers@appwise.be>
2025-05-10 15:09:14 +0200

    fix: update eslint config to include ignores for migrations

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,9 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   formatters: true,
+  ignores: [
+    'server/db/migrations',
+  ],
   rules: {
     'node/prefer-global/process': 'off',
   },
