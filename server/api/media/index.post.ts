b9e96a915746c785ddfb6e5701b65a2a7fdfb606
glenn <glenn.reumers@appwise.be>
2025-05-18 11:02:59 +0200

    feat(upload): refactor file upload handling and extract validation logic

--- server/api/media/index.post.ts
+++ server/api/media/index.post.ts
@@ -1,41 +1,35 @@
 import type { H3Event } from 'h3'
-import { createError, readMultipartFormData } from 'h3'
+import { createError } from 'h3'
 import { useMediaService } from '~~/server/services/media/mediaService'
+import { validateFileUpload } from '~~/server/validators/file/validateFileUpload'
 
 export default defineEventHandler(async (event: H3Event) => {
   const mediaService = useMediaService()
 
   try {
-    const formData = await readMultipartFormData(event)
+    const {
+      fileBuffer,
+      originalFilename,
+      mimeType,
+    } = await validateFileUpload(event)
 
-    if (!formData || formData.length === 0) {
-      throw createError({ statusCode: 400, statusMessage: 'No files uploaded' })
-    }
-
-    const fileData = formData.find(item => item.name === 'file')
-
-    if (!fileData || !fileData.data) {
-      throw createError({ statusCode: 400, statusMessage: 'No file found with the name "file"' })
-    }
-
-    const fileBuffer = fileData.data
-    const originalFilename = fileData.filename
-    const mimeType = fileData.type
-
-    if (!originalFilename) {
-      throw createError({ statusCode: 400, statusMessage: 'File must have a filename' })
-    }
-
-    const result = await mediaService.uploadAndSaveImage({ // Use the service
+    const result = await mediaService.uploadAndSaveImage({
       fileBuffer,
       originalFilename,
       mimeType,
     })
 
-    return { message: 'File uploaded and reference saved', imageId: result.imageId }
+    return {
+      message: 'File uploaded and reference saved',
+      imageId: result.imageId,
+    }
   }
-  catch (error: any) {
+  catch (error) {
     console.error('Error uploading file or saving reference:', error)
-    return createError({ statusCode: 500, statusMessage: 'Failed to upload file and save reference' })
+
+    return createError({
+      statusCode: 500,
+      statusMessage: 'Failed to upload file and save reference',
+    })
   }
 })
